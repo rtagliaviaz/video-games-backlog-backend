281ee7ef652bb657407dd7189cd33b75b00999e9
unknown <rtagliavia.pp@gmail.com>
2021-11-02 09:55:44 -0400

    fix: swtiched from local mongodb to atlas

--- src/database.js
+++ src/database.js
@@ -1,3 +1,6 @@
+// `mongodb://localhost/gamesDB`
+
+
 const mongoose = require('mongoose');
 require('dotenv');
 
@@ -7,7 +10,8 @@ require('dotenv');
       user: process.env.DB_USER,
       pass: process.env.DB_PASSWORD
     }
-    const db = await mongoose.connect(process.env.MONGODB_URI ||`mongodb://localhost/gamesDB`);
+    const db = await mongoose.connect(process.env.MONGODB_URI ||`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@devcluster.s8bmw.mongodb.net/gamesDB?retryWrites=true&w=majority`);
+    
     console.log("Database is Connected", db.connection.name);
   } catch (error) {
     console.error(error);
