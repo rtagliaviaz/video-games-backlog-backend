2ee9f05c8643c4c91a8603a1e06ba61de052fea6
unknown <rtagliavia.pp@gmail.com>
2021-11-02 11:35:10 -0400

    fixed: mongoose method at games controller

--- src/routes/games.controller.js
+++ src/routes/games.controller.js
@@ -3,7 +3,7 @@ const Game = require('./Games');
 
 gamesCtrl.getGames = async (req, res) => {
   try {
-    const games = await Game.find();
+    const games = await Game.findOne();
     return res.json(games);
   } catch (error) {
     res.json(error);
